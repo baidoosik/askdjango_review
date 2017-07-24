1e905855016fcb925acc45cb1b5e51cbde614dba
baidoosik <qoentlr37@naver.com>
2017-07-24 23:17:35 +0900

    queryset을 이용한 간다한 검색구현 post_list 화면 만들기

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from .models import Post,Article
 # Create your views here.
+def post_list(request):
+    query = Post.objects.all()
+
+    q = request.GET.get('q','')
+
+    if q:
+        query=query.filter(title__icontains=q)
+
+    return render(request,'blog/post_list.html',{
+        'post':query
+    })
