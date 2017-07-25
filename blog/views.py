03e1d44fc073bbde8a49094af175e3eed4d13c46
baidoosik <qoentlr37@naver.com>
2017-07-25 23:09:33 +0900

    template 기본 구조 잡기

--- blog/views.py
+++ blog/views.py
@@ -2,13 +2,18 @@ from django.shortcuts import render
 from .models import Post,Article
 # Create your views here.
 def post_list(request):
-    query = Post.objects.all()
+    query1 = Post.objects.all()
+
+    query2 = Article.objects.all()
 
     q = request.GET.get('q','')
 
     if q:
-        query=query.filter(title__icontains=q)
+        query1=query1.filter(title__icontains=q)
+
+        query2=query2.filter(title__icontains=q)
 
     return render(request,'blog/post_list.html',{
-        'post':query
+        'post1':query1,
+        'post2':query2,
     })
