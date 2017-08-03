96fb4ef407e02cea1c1a3cbca44ecf5bc16c60c6
baidoosik <qoentlr37@naver.com>
2017-08-04 00:54:07 +0900

    logout 뷰 구현

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
 from . import views
-from django.contrib.auth.views import login
+from django.contrib.auth.views import login,logout
+from django.conf import settings
 
 urlpatterns =[
     url(r'^signup/$',views.signup_form,name='sign_up'),
@@ -8,4 +9,6 @@ urlpatterns =[
     url(r'^login/$',login,name='login',kwargs={
         'template_name':'accounts/login.html'
         }),
+    url(r'^logout/$',logout, name='logout',
+            kwargs={'next_page': settings.LOGIN_URL}),
 ]
